d41776a0eb7493ace3a5c2b53f860050d303cae1
aaigorevich <andrewarestotel@gmail.com>
2024-08-10 22:38:52 +0700

    feat: add table and progress bar

--- SearcherCellsExcell.py
+++ SearcherCellsExcell.py
@@ -1,12 +1,15 @@
 import os
 import sys
 import openpyxl
+from tqdm import tqdm
+from prettytable import PrettyTable
+from time import sleep
 
 # Укажите путь к папке с Excel-файлами
 folder_path = os.path.abspath(os.curdir)
 
 # Подсказка для пользователей
-print("Для выхода из программы введите: searcher -stop")
+print("\nДля выхода из программы введите: searcher -stop\n")
 
 while True:
 
@@ -22,35 +25,49 @@ while True:
         store_results = []  # Список для хранения результатов поиска
 
         # Итерируемся по всем файлам в указанной папке
-        for filename in os.listdir(folder_path):
+        for filename in tqdm(
+                os.listdir(folder_path),
+                desc="Статус работы: ",
+                colour="#00FF00", position=1, leave=False
+                ):
             if filename.endswith(".xlsx"):
                 file_path = os.path.join(folder_path, filename)
-
                 # Загружаем книгу Excel
                 workbook = openpyxl.load_workbook(file_path, read_only=True)
-
                 # Итерируемся по всем листам книги
-                for sheet_name in workbook.sheetnames:
+                for sheet_name in tqdm(
+                        workbook.sheetnames,
+                        # Вывод файлов которые были просмотренны
+                        desc=f"Просмотр файла {filename}.",
+                        colour="#FFFF00",
+                        ascii=True, leave=False
+                        ):
+                    sleep(0.8)
                     sheet = workbook[sheet_name]
-
                     # Итерируемся по всем ячейкам в листе
                     for row in sheet.iter_rows():
                         for cell in row:
-                            # Проверяем, совпадает ли значение ячейки с искомыми
+                            # Проверяем
+                            # совпадает ли значение ячейки с искомыми
                             cell_value = str(cell.value)
                             if cell_value == search_value:
-                                # Сохраняем путь к файлу, имя листа и адрес ячейки
-                                store_results.append(f"ИмяФайла: {filename} || НазваниеЛиста: {sheet.title} || КоординатыЯчейки: {cell.coordinate} ||")
-                print(" => " + filename)  # Вывод файлов в которые были просмотренны
+                                # Сохраняем путь к
+                                # файлу, имени листа и адреса ячейки
+                                store_results.append(
+                                    [filename,
+                                     sheet.title,
+                                     cell.coordinate])
 
         # Возвращаем результаты поиска
         return store_results
 
     locations = find_cell_by_value(folder_path, search_value)
+    table = PrettyTable(["Имя файла", "Название Листа", "Координаты Ячейки"])
+    for row in locations:
+        table.add_row(row)
     if locations:
-        print("Данные найдены: ")
-        for location in locations:
-            print(location)
+        print("\nНайдены совпадения(в .xlsx файлах) с вашем значением: ")
+        print(table)
     else:
         print("Ячейка не найдена.")
-    print("|================================================================|")
+    print("|================================================================|\n")

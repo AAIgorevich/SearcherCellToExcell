18400eac89b7406ff9f6d1f53e9be05b6b884a8c
aaigorevich <andrewarestotel@gmail.com>
2024-09-08 22:05:23 +0700

    update: add handler KeyboardInterrupt

--- SearcherCellsExcell.py
+++ SearcherCellsExcell.py
@@ -84,65 +84,69 @@ SCE.first_init_command_help()
 # Укажите путь к папке с Excel-файлами
 folder_path = os.path.abspath(os.curdir)
 
-while True:
-    # Укажите значение, которое нужно найти
-    search_value = str(input("Ваше значение: "))
 
-    # Получение результата
-    result = SCE.call_comands(search_value)
+try:
+    while True:
+        # Укажите значение, которое нужно найти
+        search_value = str(input("Ваше значение: "))
 
-    # Остановка или продолжение использование программы
-    if result == "stop":
-        break
-    elif result == "continue":
-        continue
+        # Получение результата
+        result = SCE.call_comands(search_value)
 
-    def find_cell_by_value(folder_path, search_value: str):
-        store_results = []  # Список для хранения результатов поиска
+        # Остановка или продолжение использование программы
+        if result == "stop":
+            break
+        elif result == "continue":
+            continue
 
-        # Итерируемся по всем файлам в указанной папке
-        for filename in tqdm(
-                os.listdir(folder_path),
-                desc="Статус работы: ",
-                colour="#00FF00", position=1, leave=False
-                ):
-            if filename.endswith(".xlsx"):
-                file_path = os.path.join(folder_path, filename)
-                # Загружаем книгу Excel
-                workbook = openpyxl.load_workbook(file_path, read_only=True)
-                # Итерируемся по всем листам книги
-                for sheet_name in tqdm(
-                        workbook.sheetnames,
-                        # Вывод файлов которые были просмотренны
-                        desc=f"Просмотр файла {filename}.",
-                        colour="#FFFF00",
-                        ascii=True, leave=False
-                        ):
-                    sheet = workbook[sheet_name]
-                    # Итерируемся по всем ячейкам в листе
-                    for row in sheet.iter_rows():
-                        for cell in row:
-                            # Проверяем
-                            # совпадает ли значение ячейки с искомыми
-                            cell_value = str(cell.value)
-                            if cell_value == search_value:
-                                # Сохраняем путь к
-                                # файлу, имени листа и адреса ячейки
-                                store_results.append(
-                                    [filename,
-                                     sheet.title,
-                                     cell.coordinate])
+        def find_cell_by_value(folder_path, search_value: str):
+            store_results = []  # Список для хранения результатов поиска
 
-        # Возвращаем результаты поиска
-        return store_results
+            # Итерируемся по всем файлам в указанной папке
+            for filename in tqdm(
+                    os.listdir(folder_path),
+                    desc="Статус работы: ",
+                    colour="#00FF00", position=1, leave=False
+                    ):
+                if filename.endswith(".xlsx"):
+                    file_path = os.path.join(folder_path, filename)
+                    # Загружаем книгу Excel
+                    workbook = openpyxl.load_workbook(file_path, read_only=True)
+                    # Итерируемся по всем листам книги
+                    for sheet_name in tqdm(
+                            workbook.sheetnames,
+                            # Вывод файлов которые были просмотренны
+                            desc=f"Просмотр файла {filename}.",
+                            colour="#FFFF00",
+                            ascii=True, leave=False
+                            ):
+                        sheet = workbook[sheet_name]
+                        # Итерируемся по всем ячейкам в листе
+                        for row in sheet.iter_rows():
+                            for cell in row:
+                                # Проверяем
+                                # совпадает ли значение ячейки с искомыми
+                                cell_value = str(cell.value)
+                                if cell_value == search_value:
+                                    # Сохраняем путь к
+                                    # файлу, имени листа и адреса ячейки
+                                    store_results.append(
+                                        [filename,
+                                        sheet.title,
+                                        cell.coordinate])
 
-    locations = find_cell_by_value(folder_path, search_value)
-    table = PrettyTable(["Имя файла", "Название Листа", "Координаты Ячейки"])
-    for row in locations:
-        table.add_row(row)
-    if locations:
-        print("\nНайдены совпадения в (.xlsx) файлах с вашем значением: ")
-        print(table)
-    else:
-        print("\nДанное значение не обнаруженно в (.xlsx) файлах.")
-    print("|================================================================|")
+            # Возвращаем результаты поиска
+            return store_results
+
+        locations = find_cell_by_value(folder_path, search_value)
+        table = PrettyTable(["Имя файла", "Название Листа", "Координаты Ячейки"])
+        for row in locations:
+            table.add_row(row)
+        if locations:
+            print("\nНайдены совпадения в (.xlsx) файлах с вашем значением: ")
+            print(table)
+        else:
+            print("\nДанное значение не обнаруженно в (.xlsx) файлах.")
+        print("|===========================================================|")
+except KeyboardInterrupt:
+    SCE.command_sce_stop()
